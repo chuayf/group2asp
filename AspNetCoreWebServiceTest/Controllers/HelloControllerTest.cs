e7e16370d8fed177ce13eb7001a6042ad2e3b86e
chuayf <71249128+chuayf@users.noreply.github.com>
2020-09-17 18:22:57 +0800

    Update HelloControllerTest.cs

--- AspNetCoreWebServiceTest/Controllers/HelloControllerTest.cs
+++ AspNetCoreWebServiceTest/Controllers/HelloControllerTest.cs
@@ -25,11 +25,17 @@ namespace AspNetCoreWebServiceTest.Controllers
         }
 
         [Fact]
-        public void NoInputParamPostResponseTest()
+//         public void NoInputParamPostResponseTest()
+//         {
+//             HelloController controller = new HelloController();
+//             var response = controller.Post().Value as Response;
+//             Assert.Equal("Hello World!", response.Output);
+//         }
+         public void NoInputParamGetResponseTest()
         {
             HelloController controller = new HelloController();
-            var response = controller.Post().Value as Response;
-            Assert.Equal("Hello World!", response.Output);
+            var response = controller.Get().Value as Response;
+            Assert.Equal("Hello World!!!", response.output);
         }
 
         [Theory]
